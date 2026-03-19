8d6781b2bdbadebf168a14f322f40cff3618f7ca
cloudQuest7 <anjalijayakumar79@gmail.com>
2026-03-19 22:21:40 +0530

    build docker images

--- react-docker/src/App.tsx
+++ react-docker/src/App.tsx
@@ -16,9 +16,9 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Docker is woahhh</h1>
           <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+           yoyo<code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
         <button
